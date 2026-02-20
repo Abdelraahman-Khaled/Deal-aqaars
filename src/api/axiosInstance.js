fcb639d0431fad5c1ca6bbc5a1993cb5ae400f31
Abdelrahman Khaled <109894754+Abdelraahman-Khaled@users.noreply.github.com>
2026-02-20 17:56:57 +0200

    update

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import { toast } from "react-toastify";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+// const API_BASE_URL = "https://deal-two.vercel.app/api/";
 
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
